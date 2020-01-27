96acaf5934489afc9e7d465aa93937ad07496907
Geerson Pizarro <gerson970707620@gmail.com>
2020-01-27 13:06:11 -0500

    Conectando area con PN

--- src/main/java/com/proyecto/comunidadautonoma/model/Area.java
+++ src/main/java/com/proyecto/comunidadautonoma/model/Area.java
@@ -2,10 +2,35 @@ package com.proyecto.comunidadautonoma.model;
 
 import java.io.Serializable;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="area")
 public class Area implements Serializable{
+	
+	@Id
+	@Column(name="codca")
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Long coda;
+	
+	@Column(name="nombre")
 	private String nombre;
+	
+	@Column(name="extension")
 	private double extension;
+	
+	
+	@ManyToOne(optional=true, fetch=FetchType.EAGER)
+	@JoinColumn(name="codpn")
+	private ParqueNatural parquenatural;
 	
 	
 	public Area(String nombre, double extension) {
@@ -34,6 +59,12 @@ public class Area implements Serializable{
 	}
 	public void setExtension(double extension) {
 		this.extension = extension;
+	}
+	public ParqueNatural getParquenatural() {
+		return parquenatural;
+	}
+	public void setParquenatural(ParqueNatural parquenatural) {
+		this.parquenatural = parquenatural;
 	}
 	
 	
